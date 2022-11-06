7c0b68d70d7acac7960f427ae6295d26c57a8a22
Max Programming <ui.sabuwala@gmail.com>
2022-11-06 19:49:32 +0530

    Final Working Page

--- app.ts
+++ app.ts
@@ -1,11 +1,22 @@
 import fastify from 'fastify';
+import FastifyViewPlugin from '@fastify/view';
 import scraper from './scraper';
 
 const server = fastify();
 
+server.register(FastifyViewPlugin, {
+  engine: {
+    ejs: require('ejs'),
+  },
+});
+
 server.get('/', async (request, reply) => {
   const { attImg, evalImg } = await scraper();
-  return `Attendance: <img src="${attImg.data.data.url}" />\nEvaluation: <img src="${evalImg.data.data.url}" />`;
+  // return `Attendance: <img src="${attImg.data.data.url}" />\nEvaluation: <img src="${evalImg.data.data.url}" />`;
+  return reply.view('./templates/index.ejs', {
+    img1: attImg.data.data.url,
+    img2: evalImg.data.data.url,
+  });
 });
 
 server.listen({ port: 8080 }, (err, address) => {
